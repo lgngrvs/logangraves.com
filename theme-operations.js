e9208b9d8b188ba7a88a831fbd1d8a10ba36e91e
Logan Graves <me@logangraves.com>
2021-09-18 00:15:25 -0700

    Add finished menu animations and begin navbar

--- theme-operations.js
+++ theme-operations.js
@@ -1,14 +1,22 @@
 function menuChange() {
-    var top = document.getElementById("hamburger-top");
-    var mid = document.getElementById("hamburger-mid");
-    var bottom = document.getElementById("hamburger-bottom");
+    // Each of the three hamburger menu bars has a different animation when they get the class "is-active."
+    // for loops just go through and assign or remove the appropriate classes
+
+    var top = document.getElementById("hamburger-top"),
+        mid = document.getElementById("hamburger-mid"), 
+        bottom = document.getElementById("hamburger-bottom"),
+        nav = document.getElementById("nav")
+    var all = [top, mid, bottom, nav]
+
     if (top.classList.contains("is-active")) {
-        top.classList.remove("is-active")
-        mid.classList.remove("is-active")
-        bottom.classList.remove("is-active")
+        for (i of all) {
+            i.classList.replace("is-active", "is-inactive");
+        }
+    } else if (top.classList.contains("is-inactive")) {
+        for (i of all) {
+            i.classList.replace("is-inactive", "is-active");
+        }
     } else {
-        top.classList.add("is-active")
-        mid.classList.add("is-active")
-        bottom.classList.add("is-active")
+        console.log("This message should never appear")
     }
 }
