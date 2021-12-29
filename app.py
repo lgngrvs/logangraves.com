0fe22db2aad96c548a689ecad3f51abf2ddac152
Logan Graves <me@logangraves.com>
2021-12-28 19:08:20 -0700

    add database and sample posts + some templates

--- app.py
+++ app.py
@@ -1,7 +1,31 @@
-# from flask import Flask
+# "Flask Run" to start development server locally. 
 
-# app = Flask(__name__)
+from flask import Flask, render_template
+import sqlite3
 
-# @app.route("/")
-# def index(): 
-#     return "homepage"
+app = Flask(__name__)
+
+def get_db_connection(): 
+    connection = sqlite3.connect('database.db')
+    connection.row_factory = sqlite3.Row
+    return connection
+
+def get_posts():
+    connection = get_db_connection()
+    connection.row_factory = sqlite3.Row
+    posts = connection.execute('SELECT * FROM posts').fetchall()
+    connection.close()
+    return posts
+
+@app.route("/")
+def index():
+    posts = get_posts()
+    return render_template('index.html', posts=posts)
+
+@app.route("/posts/<i>")
+def show_post(i):
+    i = int(i)
+    posts = get_posts()
+    print(posts)
+    post = posts[i]
+    return render_template("page.html", post = post)
---
+++ init_db.py
@@ -0,0 +1,14 @@
+import sqlite3
+
+connection = sqlite3.connect("database.db")
+cursor = connection.cursor()
+
+with open("schema.sql") as file:
+    cursor.executescript(file.read())
+
+for i in range(4):
+    ins = i
+    connection.executescript(f"INSERT INTO posts (title, content) VALUES ('Title for Post {ins}', 'Content for Post {ins}')")
+
+connection.commit()
+connection.close()
