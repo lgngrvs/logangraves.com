eba1a42ee24c03391b0fa9aa4c8109af96f3e32f
Logan Graves <me@logangraves.com>
2024-03-28 09:53:21 -0700

    update post-parser-script to add more logging
    Signed-off-by: Logan Graves <me@logangraves.com>

--- post_parser_script.py
+++ post_parser_script.py
@@ -12,7 +12,8 @@ list_of_file_names = [f'{parent_directory}{file}' for file in list_of_file_names
 print(list_of_file_names)
 
 for filename in list_of_file_names: 
-    # print("running for loop")
+    print("Preparing to add post \"" + str(filename) + "\"")
+    print(list_of_file_names)
     whole_post_list = []
 
     with open(filename) as file:
@@ -28,7 +29,6 @@ for filename in list_of_file_names:
     tags = Markup.escape(whole_post_list[2][6:]).strip("\n")
     content_type = Markup.escape(whole_post_list[3][6:]).strip("\n")
     desc = Markup.escape(markdown.markdown(whole_post_list[4][6:]))
-    print(desc)
     content = Markup.escape(markdown.markdown(whole_post_list[5]))
     slug = slugify(filename)[6:][:-3]
     wordcount = len(whole_post_list[5].split(" "))
@@ -37,7 +37,7 @@ for filename in list_of_file_names:
     print("Added " + title)
     connection = sqlite3.connect("database.db")
     cursor = connection.cursor()
-    test = connection.execute(f"SELECT * FROM posts where content LIKE '{content}'").fetchone()
+    test = connection.execute(f"SELECT * FROM posts where slug LIKE '{slug}'").fetchone()
     if test == None:
         connection.execute(f"INSERT INTO posts (timestamp, type, title, slug, tags, content, description, wordcount) VALUES ('{date}', '{content_type}', '{title}', '{slug}', '{tags}', '{content}', '{desc}', '{wordcount}')")
         connection.commit()
